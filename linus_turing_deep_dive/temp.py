3821d46beed46fcd447f3a8370e476611e921f79
slowisfast <slowisfast2030@163.com>
2023-06-14 11:18:07 +0800

    slow is fast

--- linus_turing_deep_dive/temp.py
+++ linus_turing_deep_dive/temp.py
@@ -1,24 +1,47 @@
+# from manim import *
+
+# def square_animation_override(square):
+#     return GrowFromPoint(square, [-3, -2, 0])
+
+
+# class MyScene(Scene):
+#     def construct(self):
+#         square = Square()
+#         rotate_anim = GrowFromCenter(square)
+#         self.play(rotate_anim)
+#         self.wait()
+
+#         Square.animation_overrides[GrowFromCenter] = square_animation_override
+
+#         square = Square()
+#         rotate_anim = GrowFromCenter(square)
+#         self.play(rotate_anim)
+#         self.wait()
+
+# if __name__ == "__main__":
+#     with tempconfig({"quality": "low_quality", "preview": True}):
+#         scene = MyScene()
+#         scene.render()
+
+
+
 from manim import *
 
-def square_animation_override(square):
-    return GrowFromPoint(square, [-3, -2, 0])
-
-
-class MyScene(Scene):
-    def construct(self):
-        square = Square()
-        rotate_anim = GrowFromCenter(square)
-        self.play(rotate_anim)
-        self.wait()
-
-        Square.animation_overrides[GrowFromCenter] = square_animation_override
+class CustomMobject(Rectangle):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
         
-        square = Square()
-        rotate_anim = GrowFromCenter(square)
-        self.play(rotate_anim)
-        self.wait()
+        self.animation_overrides[FadeIn] = self._fade_in_override
+    
+    def _fade_in_override(self, mobject):
+        return GrowFromCenter(mobject)
+
+class Test(Scene):
+    def construct(self):
+        custom_obj = CustomMobject()
+        self.play(FadeIn(custom_obj))
 
 if __name__ == "__main__":
     with tempconfig({"quality": "low_quality", "preview": True}):
-        scene = MyScene()
+        scene = Test()
         scene.render()
--- manim/mobject/mobject.py
+++ manim/mobject/mobject.py
@@ -106,6 +106,29 @@ class Mobject:
             scene.render()
     """
 
+    """linus
+    from manim import *
+
+    class CustomMobject(Rectangle):
+        def __init__(self, **kwargs):
+            super().__init__(**kwargs)
+            
+            self.animation_overrides[FadeIn] = self._fade_in_override
+        
+        def _fade_in_override(self, mobject):
+            return GrowFromCenter(mobject)
+
+    class Test(Scene):
+        def construct(self):
+            custom_obj = CustomMobject()
+            self.play(FadeIn(custom_obj))
+
+    if __name__ == "__main__":
+        with tempconfig({"quality": "low_quality", "preview": True}):
+            scene = Test()
+            scene.render()
+    """
+
     @classmethod
     def __init_subclass__(cls, **kwargs):
         super().__init_subclass__(**kwargs)

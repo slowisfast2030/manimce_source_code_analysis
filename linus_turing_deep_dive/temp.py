6d9b6dbe092b2ffd363817ee94ce80d51f896582
slowisfast <slowisfast2030@163.com>
2023-06-14 11:04:39 +0800

    slow is fast

--- linus_turing_deep_dive/temp.py
+++ linus_turing_deep_dive/temp.py
@@ -1,39 +1,24 @@
-# from manim import *
-
-# class CircleAndSquare(Scene):
-#     def construct(self):
-#         circle = Circle(color=RED, radius=1)
-#         square = Square(color=BLUE, side_length=2)
-#         circle.shift(LEFT*3)
-#         square.shift(RIGHT*3)
-#         self.add(circle, square)
-
-#         # Set the square as the target for the circle
-#         circle.target = square
-
-#         # Create the animation
-#         self.play(MoveToTarget(circle))
-
-# with tempconfig({"quality": "medium_quality", "preview": True}):
-#     scene = CircleAndSquare()
-#     scene.render()
-
 from manim import *
+
+def square_animation_override(square):
+    return GrowFromPoint(square, [-3, -2, 0])
+
 
 class MyScene(Scene):
     def construct(self):
-        # Create a circle Mobject
-        circle = Circle()
+        square = Square()
+        rotate_anim = GrowFromCenter(square)
+        self.play(rotate_anim)
+        self.wait()
 
-        # Override the animation for the radius property
-        circle.animation_overrides["radius"] = {"run_time": 10, "rate_func": there_and_back}
-
-        # Animate the circle
-        self.play(GrowFromCenter(circle))
-
+        Square.animation_overrides[GrowFromCenter] = square_animation_override
+        
+        square = Square()
+        rotate_anim = GrowFromCenter(square)
+        self.play(rotate_anim)
         self.wait()
 
 if __name__ == "__main__":
-    with tempconfig({"quality": "high_quality", "preview": True}):
+    with tempconfig({"quality": "low_quality", "preview": True}):
         scene = MyScene()
         scene.render()

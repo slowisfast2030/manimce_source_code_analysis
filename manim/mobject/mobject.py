119f7a0c0764a8cd1309ee6014396cabdd28b1d7
slowisfast <slowisfast2030@163.com>
2023-06-14 11:06:29 +0800

    slow is fast

--- manim/mobject/mobject.py
+++ manim/mobject/mobject.py
@@ -79,6 +79,32 @@ class Mobject:
     """
 
     animation_overrides = {}
+    """linus
+    from manim import *
+
+    def square_animation_override(square):
+        return GrowFromPoint(square, [-3, -2, 0])
+
+
+    class MyScene(Scene):
+        def construct(self):
+            square = Square()
+            rotate_anim = GrowFromCenter(square)
+            self.play(rotate_anim)
+            self.wait()
+
+            Square.animation_overrides[GrowFromCenter] = square_animation_override
+            
+            square = Square()
+            rotate_anim = GrowFromCenter(square)
+            self.play(rotate_anim)
+            self.wait()
+
+    if __name__ == "__main__":
+        with tempconfig({"quality": "low_quality", "preview": True}):
+            scene = MyScene()
+            scene.render()
+    """
 
     @classmethod
     def __init_subclass__(cls, **kwargs):

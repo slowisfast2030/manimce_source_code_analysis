ef808d66f4ccbe35d8c666169cf5851b33dd4f37
slowisfast <slowisfast2030@163.com>
2023-06-17 20:40:33 +0800

    slow is fast

--- linus_turing_deep_dive/temp.py
+++ linus_turing_deep_dive/temp.py
@@ -98,3 +98,36 @@
 #     with tempconfig({"quality": "high_quality", "preview": True}):
 #         scene = TableExamples()
 #         scene.render()
+
+from manim import *
+
+class CircleAnimation(Scene):
+    def construct(self):
+        circle = Circle(radius=1, color=BLUE)
+        self.add(circle)
+
+        # Create a ValueTracker to control the scale factor
+        scale_factor = ValueTracker(0.5)
+
+        # Create an updater function that scales the circle based on the value of scale_factor
+        circle.add_updater(lambda c: c.scale(scale_factor.get_value()))
+
+        # Add the circle to the scene
+        self.add(circle)
+
+        # Animate the scale_factor from 1 to 2
+        self.play(scale_factor.animate.set_value(2), run_time=2)
+
+        # Pause for 1 second
+        self.wait(1)
+
+        # Animate the scale_factor back to 1
+        self.play(scale_factor.animate.set_value(1), run_time=2)
+
+        # Pause for 1 second
+        self.wait(1)
+
+if __name__ == "__main__":
+    with tempconfig({"quality": "high_quality", "preview": True}):
+        scene = CircleAnimation()
+        scene.render()

4b40e5476760d81141b24ca91fe0fe90f94653a9
slowisfast <slowisfast2030@163.com>
2023-06-17 22:09:50 +0800

    slow is fast

--- linus_turing_deep_dive/temp.py
+++ linus_turing_deep_dive/temp.py
@@ -109,8 +109,8 @@ class CircleAnimation(Scene):
 
         self.add(circle)
 
-        self.play(scale_factor.animate.set_value(1.1), run_time=1)
-        #self.play(scale_factor.animate.set_value(1.1), run_time=2)
+        self.play(scale_factor.animate.set_value(1.1))
+        self.play(scale_factor.animate.set_value(0.9))
     
 
 if __name__ == "__main__":

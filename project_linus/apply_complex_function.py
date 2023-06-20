ca79c5addd6e1f2e739da1f594b62265040e36aa
slowisfast <slowisfast2030@163.com>
2023-06-20 21:12:35 +0800

    做减法，极致的专注

--- project_linus/apply_complex_function.py
+++ project_linus/apply_complex_function.py
@@ -21,15 +21,21 @@ class ApplyFuncExample(Scene):
         # animations = circ.animate.apply_complex_function(
         #     lambda x: np.exp(x*1j)
         # )
+
+        # 每一维坐标全部加1
+        circ_ref = circ.copy().apply_function(
+            lambda x: x+1
+        )
+        circ_ref.set_color(YELLOW)
         animations = circ.animate.apply_function(
             lambda p: p + np.array(
                     [
                         np.sin(p[1]),
-                        np.sin(p[0]),
+                        np.cos(p[0]),
                         0,
                     ]
         ))
-        self.add(plane, circ)
+        self.add(plane, circ, circ_ref)
         self.play(animations, run_time=2)
 
 # github colpilot真厉害！下面的代码可以自动生成。

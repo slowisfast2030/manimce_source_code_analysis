63af7e6f93288cb6b9547992d7c5b273a969417c
slowisfast <slowisfast2030@163.com>
2023-07-16 11:03:48 +0800

    做减法，极致的专注

--- project_linus/become_func.py
+++ project_linus/become_func.py
@@ -1,5 +1,44 @@
 from manim import *
 
+class GetRiemannRectanglesExample(Scene):
+        def construct(self):
+            ax = Axes(y_range=[-2, 10])
+            quadratic = ax.plot(lambda x: 0.5 * x ** 2 - 0.5)
+
+            # the rectangles are constructed from their top right corner.
+            # passing an iterable to `color` produces a gradient
+            rects_right = ax.get_riemann_rectangles(
+                quadratic,
+                x_range=[-4, -3],
+                dx=0.25,
+                color=(TEAL, BLUE_B, DARK_BLUE),
+                input_sample_type="right",
+            )
+
+            # the colour of rectangles below the x-axis is inverted
+            # due to show_signed_area
+            rects_left = ax.get_riemann_rectangles(
+                quadratic, x_range=[-1.5, 1.5], dx=0.15, color=YELLOW
+            )
+
+            bounding_line = ax.plot(
+                lambda x: 1.5 * x, color=BLUE_B, x_range=[3.3, 6]
+            )
+            bounded_rects = ax.get_riemann_rectangles(
+                bounding_line,
+                bounded_graph=quadratic,
+                dx=0.15,
+                x_range=[4, 5],
+                show_signed_area=False,
+                color=(MAROON_A, RED_B, PURPLE_D),
+            )
+
+            self.add(
+                ax, bounding_line, quadratic, rects_right, rects_left, bounded_rects
+            )
+
+            self.wait(3)
+
 class myScene(Scene):
     def construct(self):
         points = [ORIGIN, UP, RIGHT, LEFT]

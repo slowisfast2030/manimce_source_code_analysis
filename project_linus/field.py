14b243a24ad226ee41bbc197c6b932143d58e3e3
Ai Chain <hitthumit@gmail.com>
2023-09-12 15:34:03 +0800

    做减法，极致的专注

---
+++ project_linus/field.py
@@ -0,0 +1,31 @@
+from manim import *
+
+class BasicUsage(Scene):
+    def construct(self):
+        func = lambda pos: ((pos[0] * UR + pos[1] * LEFT) - pos) / 3
+        self.add(ArrowVectorField(func))
+
+
+class SizingAndSpacing(Scene):
+    def construct(self):
+        func = lambda pos: np.sin(pos[0] / 2) * UR + np.cos(pos[1] / 2) * LEFT
+        vf = ArrowVectorField(func, x_range=[-7, 7, 1])
+        self.add(vf)
+        self.wait()
+
+        length_func = lambda x: x / 3
+        vf2 = ArrowVectorField(func, x_range=[-7, 7, 1], length_func=length_func)
+        self.play(vf.animate.become(vf2))
+        self.wait()
+
+
+class Coloring(Scene):
+    def construct(self):
+        func = lambda pos: pos - LEFT * 5
+        colors = [RED, YELLOW, BLUE, DARK_GRAY]
+        min_radius = Circle(radius=2, color=colors[0]).shift(LEFT * 5)
+        max_radius = Circle(radius=10, color=colors[-1]).shift(LEFT * 5)
+        vf = ArrowVectorField(
+            func, min_color_scheme_value=2, max_color_scheme_value=10, colors=colors
+        )
+        self.add(vf, min_radius, max_radius)

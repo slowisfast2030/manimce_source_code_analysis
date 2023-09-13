fc5164292fcd69ac92d6531850d35be03e519c0c
Ai Chain <hitthumit@gmail.com>
2023-09-14 00:34:43 +0800

    做减法，极致的专注

--- project_linus/field.py
+++ project_linus/field.py
@@ -33,3 +33,18 @@ class Coloring(Scene):
             func, min_color_scheme_value=2, max_color_scheme_value=10, colors=colors
         )
         self.add(vf, min_radius, max_radius)
+
+class SpawningAndFlowingArea(Scene):
+    def construct(self):
+        func = lambda pos: np.sin(pos[0]) * UR + np.cos(pos[1]) * LEFT + pos / 5
+        stream_lines = StreamLines(
+            func, x_range=[-3, 3, 0.2], y_range=[-2, 2, 0.2], padding=1
+        )
+
+        spawning_area = Rectangle(width=6, height=4)
+        flowing_area = Rectangle(width=8, height=6)
+        labels = [Tex("Spawning Area"), Tex("Flowing Area").shift(DOWN * 2.5)]
+        for lbl in labels:
+            lbl.add_background_rectangle(opacity=0.6, buff=0.05)
+
+        self.add(stream_lines, spawning_area, flowing_area, *labels)

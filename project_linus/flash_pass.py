888a8b253de69d4e0d65f94b40da9d5fd238cbdd
Ai Chain <hitthumit@gmail.com>
2023-09-13 15:39:42 +0800

    做减法，极致的专注

--- project_linus/flash_pass.py
+++ project_linus/flash_pass.py
@@ -13,3 +13,11 @@ class TimeWidthValues(Scene):
                 run_time=2,
                 time_width=time_width
             ))
+
+class ContinuousMotion(Scene):
+    def construct(self):
+        func = lambda pos: np.sin(pos[0] / 2) * UR + np.cos(pos[1] / 2) * LEFT
+        stream_lines = StreamLines(func, stroke_width=3, max_anchors_per_line=30)
+        self.add(stream_lines)
+        stream_lines.start_animation(warm_up=False, flow_speed=1.5)
+        self.wait(stream_lines.virtual_time / stream_lines.flow_speed)

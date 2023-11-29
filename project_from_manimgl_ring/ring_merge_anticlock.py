028d24c70f9a939e4f907818ef7068afbc02182f
Ai Chain <hitthumit@gmail.com>
2023-11-29 23:42:58 +0800

    完整作品

---
+++ project_from_manimgl_ring/ring_merge_anticlock.py
@@ -0,0 +1,22 @@
+from manim import *
+
+"""
+将circle的点集设置为顺时针
+会出现奇怪的渲染效果
+"""
+class test(Scene):
+    def construct(self):
+        #ring = Circle(radius = 2, n_components=4).center()
+        ring = Circle(radius = 2).center()
+        ring.set_stroke(width = 0.5)
+        ring.set_fill(RED,0.5)
+
+        # 将ring的点集变为顺时针
+        #ring.rotate(PI, RIGHT)
+
+        for index, point in enumerate(ring.points):
+            dot = Dot(point)
+            label = Text(str(index), font_size=24).next_to(dot, point-ring.get_center(), buff=0.1)
+            self.add(dot, label)
+
+        self.add(ring)

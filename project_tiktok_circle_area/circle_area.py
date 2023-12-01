66283d16c2139a93a24aae9f4234149993cddf9b
Ai Chain <hitthumit@gmail.com>
2023-12-01 21:22:57 +0800

    完整作品

--- project_tiktok_circle_area/circle_area.py
+++ project_tiktok_circle_area/circle_area.py
@@ -5,57 +5,62 @@ class ShowCreation(Create):
     pass
 
 class CircleArea(Scene):
-    def construct(self):
-        radius = 1.5
-        stroke_color = WHITE
-        fill_color = BLUE_E
-        fill_opacity = 0.75
-        circle_corner = UP + LEFT
-        radial_line_color = MAROON_B
 
+    def setup(self):
+        self.radius = 1.5
+        self.stroke_color = WHITE
+        self.fill_color = BLUE_E
+        self.fill_opacity = 0.75
+        self.circle_corner = UP + LEFT
+        self.radial_line_color = MAROON_B
 
-        circle = Circle(
-            radius = radius,
-            stroke_color = stroke_color,
-            fill_color = fill_color,
-            fill_opacity = fill_opacity,
+        self.circle = Circle(
+            radius = self.radius,
+            stroke_color = self.stroke_color,
+            fill_color = self.fill_color,
+            fill_opacity = self.fill_opacity,
         )
-        circle.to_corner(circle_corner, buff = MED_LARGE_BUFF)
+        self.circle.to_corner(self.circle_corner, buff = MED_LARGE_BUFF)
         
-        radius_line = Line(
-            circle.get_center(),
-            circle.get_right(),
-            color = radial_line_color
+        self.radius_line = Line(
+            self.circle.get_center(),
+            self.circle.get_right(),
+            color = self.radial_line_color
         )
-        radius_brace = Brace(radius_line, buff = SMALL_BUFF)
-        radius_label = radius_brace.get_tex("R", buff = SMALL_BUFF)
+        self.radius_brace = Brace(self.radius_line, buff = SMALL_BUFF)
+        self.radius_label = self.radius_brace.get_tex("R", buff = SMALL_BUFF)
 
-        radius_group = VGroup(
-            radius_line, radius_brace, radius_label
+        self.radius_group = VGroup(
+            self.radius_line, self.radius_brace, self.radius_label
         )
+        self.add(self.circle, *self.radius_group)
+        
 
-        #self.add(circle, radius_group)
-
+    def construct(self):
+        self.introduce_circle()
+        
+    def introduce_circle(self):
+        self.remove(self.circle)
         self.play(
-            ShowCreation(radius_line),
-            GrowFromCenter(radius_brace),
-            Write(radius_label),
+            ShowCreation(self.radius_line),
+            GrowFromCenter(self.radius_brace),
+            Write(self.radius_label),
         )
-        circle.set_fill(opacity = 0)
+        self.circle.set_fill(opacity = 0)
 
         self.play(
             Rotate(
-                radius_line, 2*np.pi-0.001, 
-                about_point = circle.get_center(),
+                self.radius_line, 2*PI-0.001, 
+                about_point = self.circle.get_center(),
             ),
-            ShowCreation(circle),
+            ShowCreation(self.circle),
             run_time = 2
         )
 
-        self.bring_to_front(radius_group)
+        self.bring_to_front(self.radius_group)
 
         self.play(
-            circle.animate.set_fill(fill_color, fill_opacity)
+            self.circle.animate.set_fill(self.fill_color, self.fill_opacity)
         )
 
         

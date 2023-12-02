e563ced8f8140cb052b537770d07afe9ec62a172
Ai Chain <hitthumit@gmail.com>
2023-12-02 23:14:19 +0800

    完整作品

--- project_tiktok_circle_area/sector_sum.py
+++ project_tiktok_circle_area/sector_sum.py
@@ -7,6 +7,13 @@ Sector Sum:
 from manim import *
 import itertools as it
 
+# 下面这几行设置竖屏
+config.frame_width = 9
+config.frame_height = 16
+
+config.pixel_width = 1080
+config.pixel_height = 1920
+
 class ShowCreation(Create):
     pass
 

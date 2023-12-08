48dfeff0270557f2fe02e49be64112f82df3e192
Ai Chain <hitthumit@gmail.com>
2023-12-08 19:22:03 +0800

    产品方法论: 用户 + 迭代

--- project_tan/s2.py
+++ project_tan/s2.py
@@ -10,12 +10,22 @@ config.frame_height = 16
 config.pixel_width = 1080
 config.pixel_height = 1920
 
-class s1(Scene):
+class s2(Scene):
     def setup(self):
         pass
 
     def construct(self):
+        self.diameter_angle()
         pass
 
     # 直径所对的圆周角是直角
-    
+    def diameter_angle(self):
+        circle = Circle(radius=2, color=RED, stroke_width=2)
+        self.play(Write(circle), run_time=1)
+        self.wait()
+
+        diameter = Line(LEFT*2, RIGHT*2, color=BLUE)
+        self.play(Write(diameter), run_time=1)
+        self.wait()
+
+        

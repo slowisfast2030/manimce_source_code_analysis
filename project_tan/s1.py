06e27953bf0ae227f00c1d3832f99e6a4b34d288
Ai Chain <hitthumit@gmail.com>
2023-12-09 01:26:33 +0800

    产品方法论: 用户 + 迭代

--- project_tan/s1.py
+++ project_tan/s1.py
@@ -112,10 +112,9 @@ class s1(Scene):
         model_2 = self.get_model_2()
         model_3 = self.get_model_3()
         model_4 = self.get_model_4()
-
         model_12 = VGroup(model_1, model_2).arrange(RIGHT, buff=1)
         model_34 = VGroup(model_3, model_4).arrange(RIGHT, buff=1)
-        model_1234 = VGroup(model_12, model_34).arrange(DOWN, buff=2).scale(0.65)
+        model_1234 = VGroup(model_12, model_34).arrange(DOWN, buff=2).scale(0.6)
 
         self.remove(self.line_gr)
         self.add(model_1234)

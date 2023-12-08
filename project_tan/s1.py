92845433eaf75d63e726a43abc5bc3853f70c776
Ai Chain <hitthumit@gmail.com>
2023-12-08 11:43:10 +0800

    产品方法论: 用户 + 迭代

--- project_tan/s1.py
+++ project_tan/s1.py
@@ -2,10 +2,13 @@ from manim import *
 
 class s1(Scene):
     def construct(self):
+        self.introduce_triangle()
         pass
 
     # 引入三角形
     def introduce_triangle(self):
-        triangle = Triangle()
-        self.play(Write(triangle))
+        triangle = Polygon([-4,0,0],
+                           [0,0,0],
+                           [0,3,0])
+        self.play(Write(triangle), run_time=2)
         self.wait()

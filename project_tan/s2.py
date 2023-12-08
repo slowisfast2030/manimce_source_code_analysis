a6d4344f0c464122db789f0dbab0d4293c425636
Ai Chain <hitthumit@gmail.com>
2023-12-08 19:30:17 +0800

    产品方法论: 用户 + 迭代

--- project_tan/s2.py
+++ project_tan/s2.py
@@ -10,8 +10,15 @@ config.frame_height = 16
 config.pixel_width = 1080
 config.pixel_height = 1920
 
+# 一个很聪明的方案
+class ShowCreation(Create):
+    pass
+
 class s2(Scene):
     def setup(self):
+        self.radius = 2
+        self.stroke_color = WHITE
+        self.radial_line_color = MAROON_B
         pass
 
     def construct(self):
@@ -20,12 +27,39 @@ class s2(Scene):
 
     # 直径所对的圆周角是直角
     def diameter_angle(self):
-        circle = Circle(radius=2, color=RED, stroke_width=2)
-        self.play(Write(circle), run_time=1)
-        self.wait()
+        # circle = Circle(radius=2, color=RED, stroke_width=2)
+        # self.play(Write(circle), run_time=1)
+        # self.wait()
 
-        diameter = Line(LEFT*2, RIGHT*2, color=BLUE)
-        self.play(Write(diameter), run_time=1)
-        self.wait()
-
+        # diameter = Line(LEFT*2, RIGHT*2, color=BLUE)
+        # self.play(Write(diameter), run_time=1)
+        # self.wait()
+        self.circle = Circle(
+            radius = self.radius,
+            stroke_color = self.stroke_color
+        )
         
+        self.radius_line = Line(
+            self.circle.get_center(),
+            self.circle.get_right(),
+            color = self.radial_line_color
+        )
+        
+        self.radius_brace = Brace(self.radius_line, buff = SMALL_BUFF)
+        self.radius_label = self.radius_brace.get_tex("R", buff = SMALL_BUFF)
+
+        self.play(
+            ShowCreation(self.radius_line),
+            GrowFromCenter(self.radius_brace),
+            Write(self.radius_label),
+        )
+        self.circle.set_fill(opacity = 0)
+
+        self.play(
+            Rotate(
+                self.radius_line, 2*PI-0.001, 
+                about_point = self.circle.get_center(),
+            ),
+            ShowCreation(self.circle),
+            run_time = 2
+        )

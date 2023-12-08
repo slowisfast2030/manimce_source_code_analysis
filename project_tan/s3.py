d4eae98be154b5e8a0757672d4eb7e82359141e3
Ai Chain <hitthumit@gmail.com>
2023-12-08 21:44:59 +0800

    产品方法论: 用户 + 迭代

---
+++ project_tan/s3.py
@@ -0,0 +1,22 @@
+from manim import *
+
+"""
+坐标系
+"""
+# 下面这几行设置竖屏
+config.frame_width = 9
+config.frame_height = 16
+
+config.pixel_width = 1080
+config.pixel_height = 1920
+
+# 一个很聪明的方案
+class ShowCreation(Create):
+    pass
+
+class s3(Scene):
+    def setup(self):
+        pass
+
+    def construct(self):
+        pass

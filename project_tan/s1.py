3ebc2b7080ed72fca9673493d7680cc5031cd19d
Ai Chain <hitthumit@gmail.com>
2023-12-08 11:47:47 +0800

    产品方法论: 用户 + 迭代

--- project_tan/s1.py
+++ project_tan/s1.py
@@ -7,8 +7,15 @@ class s1(Scene):
 
     # 引入三角形
     def introduce_triangle(self):
-        triangle = Polygon([-4,0,0],
-                           [0,0,0],
-                           [0,3,0])
+        coord_c = [-4,0,0]
+        coord_a = [0,0,0]
+        coord_b = [0,3,0]
+        triangle = Polygon(coord_c, coord_a, coord_b, color=RED)
         self.play(Write(triangle), run_time=2)
-        self.wait()
+
+        ver_c = Tex("C", color=RED).next_to(coord_c, DOWN)
+        ver_a = Tex("A", color=RED).next_to(coord_a, DOWN)
+        ver_b = Tex("B", color=RED).next_to(coord_b, RIGHT)
+        ver_ani = list(map(FadeIn, [ver_c, ver_a, ver_b]))
+
+        self.play(*ver_ani, run_time=2)

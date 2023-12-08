86239fca22737977a9525108fc9278f78423ecdf
Ai Chain <hitthumit@gmail.com>
2023-12-09 00:55:32 +0800

    产品方法论: 用户 + 迭代

--- project_tan/s1.py
+++ project_tan/s1.py
@@ -18,32 +18,28 @@ class s1(Scene):
         self.coord_d = [0, 4/3, 0]
         self.coord_e = [-4/5, 12/5, 0]
 
+        self.line_color = MAROON_B
+        self.label_color = WHITE
+
     def construct(self):
-        # self.introduce_triangle()
+        self.introduce_triangle()
         # self.introduce_half_angle()
         # self.tri_flip()
         # self.clear()
-        self.introduce_four_half_angle_model()
+        # self.introduce_four_half_angle_model()
         pass
 
     # 引入三角形
     def introduce_triangle(self):
-        coord_c = [-4,0,0]
-        coord_a = [0,0,0]
-        coord_b = [0,3,0]
-        triangle = Polygon(coord_c, coord_a, coord_b, color=RED)
+        triangle = Polygon(self.coord_c, self.coord_a, self.coord_b, color=self.line_color)
         self.play(Write(triangle), run_time=2)
 
-        ver_c = MathTex("C", color=RED).next_to(coord_c, DOWN)
-        ver_a = MathTex("A", color=RED).next_to(coord_a, DOWN)
-        ver_b = MathTex("B", color=RED).next_to(coord_b, RIGHT)
+        ver_c = MathTex("C", color=self.label_color).next_to(self.coord_c, DOWN)
+        ver_a = MathTex("A", color=self.label_color).next_to(self.coord_a, DOWN)
+        ver_b = MathTex("B", color=self.label_color).next_to(self.coord_b, RIGHT)
         ver_ani = list(map(FadeIn, [ver_c, ver_a, ver_b]))
 
         self.play(*ver_ani, run_time=1)
-
-        self.coord_c = coord_c
-        self.coord_a = coord_a
-        self.coord_b = coord_b
     
     # 引入半角
     def introduce_half_angle(self):

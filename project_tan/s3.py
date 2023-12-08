d56fb59bc8a11ae4dc59289974d739aaaecd0b86
Ai Chain <hitthumit@gmail.com>
2023-12-08 22:12:48 +0800

    产品方法论: 用户 + 迭代

--- project_tan/s3.py
+++ project_tan/s3.py
@@ -29,6 +29,7 @@ class s3(Scene):
     def construct(self):
         self.introduce_triangle()
         self.introduce_coordinate()
+        self.two_geometry()
         pass
         
     def introduce_triangle(self):
@@ -47,7 +48,7 @@ class s3(Scene):
     # 添加坐标系
     def introduce_coordinate(self):
         plane = NumberPlane()
-        self.play(Write(plane), run_time=1)
+        self.play(Write(plane), run_time=2)
 
         # 显示角平分线
         half_line = Line(self.coord_c, self.coord_d, color=self.line_color)
@@ -66,7 +67,13 @@ class s3(Scene):
     # 以费马点的例子介绍两种几何
     def two_geometry(self):
         self.clear()
+        c_a = [-1.2, -0.8, 0]
+        c_b = [1, -1.2, 0]
+        c_c = [0, 1.2, 0]
+        triangle = Polygon(c_a, c_b, c_c, color=self.line_color, stroke_width=3)
+        self.play(Write(triangle), run_time=1)
 
-        
+
+
 
 

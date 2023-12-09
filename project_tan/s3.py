81d3f75dce372191f12d7be716a23619d006897d
Ai Chain <hitthumit@gmail.com>
2023-12-10 01:54:41 +0800

    产品方法论: 用户 + 迭代

--- project_tan/s3.py
+++ project_tan/s3.py
@@ -101,11 +101,33 @@ class s3(Scene):
 
 class pr(s3):
     def construct(self):
-        #self.two_geometry()
+
+        self.two_geometry() 
+        self.two_geometry_example()
         pass
 
-        # 以费马点的例子介绍两种几何
+    # 引入两种几何
     def two_geometry(self):
+        svg_compass = SVGMobject("compass.svg").set_color(GREEN_B)
+        svg_ruler = SVGMobject("ruler.svg").set_color(TEAL_E).match_height(svg_compass)
+        svg_gr = VGroup(svg_ruler, svg_compass).arrange(RIGHT, buff=0.5).scale(1.5)
+
+        # 解析几何
+        plane = NumberPlane().scale(0.5)
+
+        # 整体
+        geo_gr = VGroup(svg_gr, plane).arrange(DOWN, buff=3)
+
+        # Display the image
+        self.play(Write(svg_gr[0]),
+                  Write(svg_gr[1]),
+                  Write(plane),
+                  run_time = 2)
+        self.wait(2)
+        pass
+
+    # 以费马点的例子介绍两种几何
+    def two_geometry_example(self):
         self.clear()
         c_a = [-1.4, -1, 0]
         c_b = [1.1, -1.4, 0]

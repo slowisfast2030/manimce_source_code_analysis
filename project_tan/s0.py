90847d9600bf51d789f10b8e92e69412ab708d6b
Ai Chain <hitthumit@gmail.com>
2023-12-09 12:56:58 +0800

    产品方法论: 用户 + 迭代

--- project_tan/s0.py
+++ project_tan/s0.py
@@ -22,3 +22,27 @@ class s0(Scene):
         pass
 
     # 屏幕从上至下出现3种解法
+    def introduce_three_methods(self):
+        
+        # self.introduce_first_method()
+        # self.introduce_second_method()
+        # self.introduce_third_method()
+
+        pass    
+
+    # 第一种解法
+    def introduce_first_method(self):
+
+        pass
+
+    # 第二种解法
+    def introduce_second_method(self):
+
+        pass
+
+    # 第三种解法    
+    def introduce_third_method(self):
+
+        pass
+    
+    

cfbda30c6efcbad4eb890d527eebe9ed2b4a6836
Ai Chain <hitthumit@gmail.com>
2023-12-09 12:53:09 +0800

    产品方法论: 用户 + 迭代

---
+++ project_tan/s0.py
@@ -0,0 +1,22 @@
+from manim import *
+
+"""
+开场
+"""
+# 下面这几行设置竖屏
+config.frame_width = 9
+config.frame_height = 16
+
+config.pixel_width = 1080
+config.pixel_height = 1920
+
+# 一个很聪明的方案
+class ShowCreation(Create):
+    pass
+
+class s0(Scene):
+    def setup(self):
+        pass
+
+    def construct(self):
+        pass

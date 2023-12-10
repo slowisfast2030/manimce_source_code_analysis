a1778392550ae747730e9c43e85c26ad7d828cd0
Ai Chain <hitthumit@gmail.com>
2023-12-11 03:49:56 +0800

    产品方法论: 用户 + 迭代

---
+++ project_tan/numberplane.py
@@ -0,0 +1,21 @@
+from manim import *
+
+# 下面这几行设置竖屏
+config.frame_width = 9
+config.frame_height = 16
+
+config.pixel_width = 1080
+config.pixel_height = 1920
+
+
+class test(Scene):
+    def construct(self):
+        number_plane = NumberPlane(
+            background_line_style={
+                "stroke_color": TEAL,
+                "stroke_width": 4,
+                "stroke_opacity": 0.6
+            }
+        )
+        self.play(Write(number_plane))
+        self.wait()

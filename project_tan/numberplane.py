eb1c2aad5dcaccb49c413a96307e544c79e27fb7
Ai Chain <hitthumit@gmail.com>
2023-12-11 03:54:29 +0800

    产品方法论: 用户 + 迭代

--- project_tan/numberplane.py
+++ project_tan/numberplane.py
@@ -7,7 +7,6 @@ config.frame_height = 16
 config.pixel_width = 1080
 config.pixel_height = 1920
 
-
 class test(Scene):
     def construct(self):
         number_plane = NumberPlane(
@@ -19,3 +18,39 @@ class test(Scene):
         )
         self.play(Write(number_plane))
         self.wait()
+
+class NumberPlaneScaled(Scene):
+    def construct(self):
+        number_plane = NumberPlane(
+            x_range=(-4, 11, 1),
+            y_range=(-3, 3, 1),
+            x_length=5,
+            y_length=2,
+        ).move_to(LEFT*3)
+
+        number_plane_scaled_y = NumberPlane(
+            x_range=(-4, 11, 1),
+            x_length=5,
+            y_length=4,
+        ).move_to(RIGHT*3)
+
+        self.add(number_plane)
+        self.add(number_plane_scaled_y)
+
+
+class NumberPlaneExample(Scene):
+    def construct(self):
+        plane = NumberPlane(
+            x_range=[-10, 10, 1],
+            y_range=[-10, 10, 1],
+            axis_config={"color": BLUE},
+            x_axis_config={
+                "numbers_to_include": range(-10, 11, 2),
+                "numbers_with_elongated_ticks": range(-10, 11, 2),
+            },
+            y_axis_config={
+                "numbers_to_include": range(-10, 11, 2),
+                "numbers_with_elongated_ticks": range(-10, 11, 2),
+            },
+        )
+        self.add(plane)

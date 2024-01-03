930fd5bbb0283397a43bf55ea37136da87990188
Ai Chain <hitthumit@gmail.com>
2024-01-03 15:23:11 +0800

    借势 + 极致的具体 深入理解海参哥说的抄袭

--- project_tiktok_circle_area/test_riemann.py
+++ project_tiktok_circle_area/test_riemann.py
@@ -13,10 +13,12 @@ class GetRiemannRectanglesExample(Scene):
             x_axis_config={
                 "include_numbers": True,
                 "numbers_to_include": [0, 1,2,3,4],  # 在x轴上只显示0, 5, 10
+                "include_ticks": True
             },
             y_axis_config={
                 "include_numbers": True,
                 "numbers_to_include": [0, 5, 10,15,20],  # 在y轴上也只显示0, 5, 10
+                "numbers_with_elongated_ticks": [0, 5, 10,15,20],
                 "decimal_number_config": {
                     "num_decimal_places": 0,  # 设置为0以显示整数
                     "color": BLUE,  # 设置x轴数字的颜色
@@ -28,13 +30,28 @@ class GetRiemannRectanglesExample(Scene):
         #ax.set_color(GRAY)
         ax.set_opacity(1)
         # 绘制函数
-        quadratic = ax.plot(lambda x: 2*PI*x, x_range=[0, 3], color=BLUE, stroke_width=2)
+        quadratic = ax.plot(lambda x: 2*PI*x, x_range=[0, 3.5], color=BLUE, stroke_width=2)
 
         # 获取黎曼矩形
         rects_left = ax.get_riemann_rectangles(
             quadratic, x_range=[0, 3], dx=0.1, color=[BLUE, GREEN]
         )
 
+        # 创建x轴标签并指定位置
+        x_label = ax.get_x_axis_label("r", direction=UP + RIGHT, buff=0.05)
+
+
         self.add(
-            ax, rects_left, quadratic
+            ax, rects_left, quadratic, x_label
         )
+
+        thinner_rects_list = [
+            ax.get_riemann_rectangles(
+                quadratic,
+                x_range=[0,3]
+                dx = 1./(10*2**n),
+                stroke_width = 1./(2**n),
+                color=[BLUE, GREEN]
+            )
+            for n in range(1, 5)
+        ]

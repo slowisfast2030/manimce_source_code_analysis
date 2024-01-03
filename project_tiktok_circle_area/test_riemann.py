8db1918dcb5fad38a63afaae8f0c940a2f0d0755
Ai Chain <hitthumit@gmail.com>
2024-01-03 15:03:26 +0800

    借势 + 极致的具体 深入理解海参哥说的抄袭

--- project_tiktok_circle_area/test_riemann.py
+++ project_tiktok_circle_area/test_riemann.py
@@ -4,11 +4,12 @@ class GetRiemannRectanglesExample(Scene):
     def construct(self):
         # 创建坐标轴并设置x轴和y轴的配置
         ax = Axes(
-            x_range=[-0.5, 4, 1],  # 从-0.5开始以确保0会显示
+            x_range=[-0.1, 4, 1],  # 从-0.5开始以确保0会显示
             y_range=[-0.5, 20, 2.5],  # 从-0.5开始以确保0会显示
             x_length=6,
             y_length=6,
             tips=False,
+            axis_config={"color": GRAY},  # 将坐标轴线条设置为灰色
             x_axis_config={
                 "include_numbers": True,
                 "numbers_to_include": [0, 1,2,3,4],  # 在x轴上只显示0, 5, 10
@@ -16,10 +17,15 @@ class GetRiemannRectanglesExample(Scene):
             y_axis_config={
                 "include_numbers": True,
                 "numbers_to_include": [0, 5, 10,15,20],  # 在y轴上也只显示0, 5, 10
-            }
+                "decimal_number_config": {
+                    "num_decimal_places": 0,  # 设置为0以显示整数
+                },
+            },
         )
         
-        ax.set_opacity(0.5)
+        # 设置坐标轴的颜色为灰色
+        #ax.set_color(GRAY)
+        ax.set_opacity(1)
         # 绘制函数
         quadratic = ax.plot(lambda x: 2*PI*x, x_range=[0, 3], color=TEAL, stroke_width=2)
 

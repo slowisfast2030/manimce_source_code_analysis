2b9325df248fffb71f50c0226ba09b90525c4e00
Ai Chain <hitthumit@gmail.com>
2024-01-03 20:58:09 +0800

    借势 + 极致的具体 ---> 深入理解海参哥说的抄袭

--- project_tiktok_circle_area/test_ring_rect.py
+++ project_tiktok_circle_area/test_ring_rect.py
@@ -4,6 +4,11 @@ class ring2rect(Scene):
     def construct(self):
         ring = self.get_ring(radius = 1, dR = 0.1)
         self.add(ring)
+        rect = self.get_rect(ring)  
+        self.add(rect)
+
+        self.play(MoveToTarget(ring))
+        self.wait()
         pass
 
     def get_ring(self, radius, dR, color = GREEN):
@@ -23,8 +28,8 @@ class ring2rect(Scene):
         # 经过打印后发现点集的数目是32，不是64
         # 奇怪：为何一开始写64呢？
         # 圆是由8段圆弧拼接而成，每一段圆弧由4个点构成
-        outer_circle = Circle(radius=radius+dR).rotate(PI).get_points()[:28]
-        inner_circle = Circle(radius=radius).rotate(PI).get_points()[:28][::-1]
+        outer_circle = Circle(radius=radius+dR).rotate(0*PI).get_points()[4:28]
+        inner_circle = Circle(radius=radius).rotate(0*PI).get_points()[4:28][::-1]
 
         # 遵守manimce的约定，每一段贝塞尔曲线由4个点构成
         line1 = [outer_circle[-1], 
@@ -61,3 +66,37 @@ class ring2rect(Scene):
         ring.dR = dR
 
         return ring
+    
+    def get_rect(self, ring: VMobject):
+        # 内外环一共16段曲线，外加两条直线，一共18段
+        n_anchors = ring.get_num_curves()
+        print(n_anchors)            
+        target = VMobject()
+        target.set_points_as_corners([
+            interpolate(ORIGIN,  DOWN, a)
+            for a in np.linspace(0, 1, n_anchors//2)
+        ]+[
+            interpolate(DOWN+RIGHT, RIGHT, a)
+            for a in np.linspace(0, 1, n_anchors//2)
+        ])
+        # 将折线闭合
+        #target.add_line_to(ORIGIN)    
+        line = [target.get_points()[-1], 
+                interpolate(target.get_points()[-1], target.get_points()[0], 0.3),
+                interpolate(target.get_points()[-1], target.get_points()[0], 0.6),
+                target.get_points()[0]] 
+        target.append_points(line)
+
+
+        target.stretch_to_fit_height(2*PI*ring.R)
+        target.stretch_to_fit_width(ring.dR)
+        target.move_to(RIGHT*2)
+        target.set_stroke(BLACK, 1)
+        target.set_fill(ring.get_fill_color(), 1)
+
+        print(len(ring.get_points()))
+        print(len(target.get_points()))
+
+        ring.target = target
+        ring.original_ring = ring.copy()
+        return target

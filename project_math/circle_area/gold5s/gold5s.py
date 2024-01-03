efe836b7b7993d950b48dd6c139fa06e2e1f7a25
Ai Chain <hitthumit@gmail.com>
2024-01-04 02:05:13 +0800

    何谓伟大？？？ 战神雷军 + 借势 + 极致的具体 ---> 深入理解海参哥说的抄袭

--- project_math/circle_area/gold5s/gold5s.py
+++ project_math/circle_area/gold5s/gold5s.py
@@ -70,7 +70,7 @@ class rings2rects(Scene):
             "run_time" : 3,
             "lag_ratio" : 0.1
         }
-        self.rings_again = VGroup(*reversed(self.get_rings()))
+        self.rings_again = VGroup(*reversed(self.get_rings())).rotate(PI/2)
         self.add(self.rings_again)
         self.play(FadeOut(self.rings),
                   FadeOut(self.ax_rects_curve),

2d4366415f45b7bd8f12e1d0dff5f411cb1c8ff2
Ai Chain <hitthumit@gmail.com>
2024-01-03 22:17:06 +0800

    何谓伟大？？？ 战神雷军 + 借势 + 极致的具体 ---> 深入理解海参哥说的抄袭

--- project_tiktok_circle_area/test_ring_rects.py
+++ project_tiktok_circle_area/test_ring_rects.py
@@ -92,3 +92,44 @@ class rings2rects(Scene):
             for radius, color in zip(radii, colors)
         ])
         return rings
+    
+    def get_rects(self, **kwargs):
+        # 创建坐标轴并设置x轴和y轴的配置
+        ax = Axes(
+            x_range=[-0.1, 4, 1],  # 从-0.5开始以确保0会显示
+            y_range=[-0.5, 20, 2.5],  # 从-0.5开始以确保0会显示
+            x_length=6,
+            y_length=6,
+            tips=False,
+            axis_config={"color": GRAY},  # 将坐标轴线条设置为灰色
+            x_axis_config={
+                "include_numbers": True,
+                "numbers_to_include": [0, 1,2,3,4],  # 在x轴上只显示0, 5, 10
+                "include_ticks": True,
+                "decimal_number_config": {
+                    "num_decimal_places": 0,  # 设置为0以显示整数
+                    "color": BLUE,  # 设置x轴数字的颜色
+                },
+            },
+            y_axis_config={
+                "include_numbers": True,
+                "numbers_to_include": [0, 5, 10,15,20],  # 在y轴上也只显示0, 5, 10
+                "numbers_with_elongated_ticks": [0, 5, 10,15,20],
+                "decimal_number_config": {
+                    "num_decimal_places": 0,  # 设置为0以显示整数
+                    "color": BLUE,  # 设置x轴数字的颜色
+                },
+            },
+        )
+        
+        # 绘制函数
+        quadratic = ax.plot(lambda x: 2*PI*x, x_range=[0, 3.5], color=BLUE, stroke_width=2)
+
+        # 获取黎曼矩形
+        rects_left = ax.get_riemann_rectangles(
+            quadratic, x_range=[0, 3.5], dx=0.1, color=[BLUE, GREEN]
+        )
+
+        self.add(
+            ax, rects_left, quadratic
+        )

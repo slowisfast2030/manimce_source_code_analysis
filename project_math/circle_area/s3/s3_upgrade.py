45f5be5304d5bf2e3a796750821f565368cd6445
Ai Chain <hitthumit@gmail.com>
2024-01-04 23:35:28 +0800

    何谓伟大？？？ 战神雷军 + 借势 + 极致的具体 ---> 深入理解海参哥说的抄袭

--- project_math/circle_area/s3/s3_upgrade.py
+++ project_math/circle_area/s3/s3_upgrade.py
@@ -54,7 +54,19 @@ class s3(Scene):
         self.add(self.circle, *self.radius_group)
 
     def construct(self):
+        """
+        分镜1:
+        画出圆, 抛出问题
+        """
         self.introduce_circle()
+        self.wait()
+        """
+        分镜2:
+        显示分割
+        """
+        self.bring_to_front(self.radius_group)
+        self.slice_into_rings()
+
         pass
 
     def introduce_circle(self):
@@ -80,8 +92,94 @@ class s3(Scene):
         self.bring_to_front(self.radius_group)
 
         circle_text = Text("如何分割圆？").set_color(BLUE).scale(0.8)
-        circle_text.to_corner(UP, buff = MED_LARGE_BUFF*4)
+        circle_text.to_corner(UP, buff = MED_LARGE_BUFF*6)
         self.play(
             self.circle.animate.set_fill(self.fill_color, self.fill_opacity),
             Write(circle_text)
         )
+
+        
+    def slice_into_rings(self):
+        rings = self.get_rings()
+        rings.set_stroke(BLACK, 1)
+
+        self.play(
+            FadeIn(
+                rings,
+                lag_ratio = 0.5,
+                run_time = 1
+            )
+        )
+        self.wait(1)
+        self.rings = rings
+        
+
+    def get_ring(self, radius, dR, color = GREEN):
+        ring = VMobject()
+        """
+        为何要旋转90度
+        当旋转90度后, 圆的起点正好在正上方
+        后续需要将这个圆环展开横向的长条, 开口放在正上方比较好看
+
+        如果不旋转, 就会将开口放在右侧
+        这样比较适合展开为竖直的长条
+
+        以cairo作为后端
+        circle的点集数目是多少?
+        """
+        #print(len(Circle(radius=radius+dR).rotate(PI/2).get_points()))
+        # 经过打印后发现点集的数目是32，不是64
+        # 奇怪：为何一开始写64呢？
+        # 圆是由8段圆弧拼接而成，每一段圆弧由4个点构成
+        outer_circle = Circle(radius=radius+dR).rotate(1*PI/2).get_points()[:]
+        inner_circle = Circle(radius=radius).rotate(1*PI/2).get_points()[:][::-1]
+
+        # 遵守manimce的约定，每一段贝塞尔曲线由4个点构成
+        line1 = [outer_circle[-1], 
+                 interpolate(outer_circle[-1], inner_circle[0], 0.3),
+                 interpolate(outer_circle[-1], inner_circle[0], 0.6),
+                 inner_circle[0]]
+        
+        line2 = [inner_circle[-1],
+                interpolate(inner_circle[-1], outer_circle[0], 0.3),
+                interpolate(inner_circle[-1], outer_circle[0], 0.6),
+                outer_circle[0]]
+        
+        """
+        将内环和外环的点集
+        加上连接内外环的直线
+        一起作为新的点集
+        """
+        points_to_add = list(outer_circle) + line1 + list(inner_circle) + line2
+
+        ring.append_points(points_to_add)
+        ring.set_stroke(width = 0)
+        """
+        我非常奇怪的一点:
+        这样人工构造出了点集之后
+        着色的时候是如何区分区域的内外呢？
+
+        cairo后端似乎很容易
+        opengl后端不容易理解
+        """
+        ring.set_fill(color, opacity = 1)
+        # 在这里明确了ring的位置
+        ring.move_to(self.circle)
+        ring.R = radius 
+        ring.dR = dR
+
+        return ring
+
+    def get_rings(self, **kwargs):
+        dR = kwargs.get("dR", self.dR)
+        print(dR)
+        colors = kwargs.get("colors", self.ring_colors)
+        radii = np.arange(0, self.radius, dR)
+        colors = color_gradient(colors, len(radii))
+
+        rings = VGroup(*[
+            self.get_ring(radius, dR = dR, color = color)
+            for radius, color in zip(radii, colors)
+        ])
+        return rings
+

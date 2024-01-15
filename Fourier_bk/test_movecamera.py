13f161f52a454452c05010cbf71d68bb4843280d
Ai Chain <hitthumit@gmail.com>
2024-01-15 13:35:21 +0800

    做好一件事 = 80%的执行套路 + 20%的创造力

--- Fourier_bk/test_movecamera.py
+++ Fourier_bk/test_movecamera.py
@@ -11,18 +11,18 @@ class CameraFocusExample(MovingCameraScene):
         self.add(square, circle, triangle)
 
         # Focus on the square
-        self.play(self.camera.frame.animate.move_to(square).set(width=square.width * 1.2))
+        self.play(self.camera.frame.animate.move_to(square).set(width=square.width * 5))
 
         # Wait and then focus on the circle
         self.wait(1)
-        self.play(self.camera.frame.animate.move_to(circle).set(width=circle.width * 1.2))
+        self.play(self.camera.frame.animate.move_to(circle).set(width=circle.width * 5))
 
         # Wait and then focus on the triangle
         self.wait(1)
-        self.play(self.camera.frame.animate.move_to(triangle).set(width=triangle.width * 1.2))
+        self.play(self.camera.frame.animate.move_to(triangle).set(width=triangle.width * 5))
 
         # Zoom out to show all objects
         self.wait(1)
-        self.play(self.camera.frame.animate.set(width=10))
+        self.play(self.camera.frame.animate.move_to(square).set(width=10))
 
         self.wait(1)

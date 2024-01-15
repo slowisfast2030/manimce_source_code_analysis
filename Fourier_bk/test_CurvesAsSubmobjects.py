14b17e004e94cc7dba83ebf9d9d519ae0cf58578
Ai Chain <hitthumit@gmail.com>
2024-01-16 01:09:14 +0800

    做好一件事 = 80%的执行套路 + 20%的创造力

--- Fourier_bk/test_CurvesAsSubmobjects.py
+++ Fourier_bk/test_CurvesAsSubmobjects.py
@@ -2,10 +2,12 @@ from manim import *
 
 class LineGradientExample(Scene):
     def construct(self):
-        curve = ParametricFunction(lambda t: [t, np.sin(t), 0], t_range=[-PI, PI, 0.001], stroke_width=10)
+        curve = ParametricFunction(lambda t: [t, np.sin(t), 0], t_range=[-PI, PI, 0.01], stroke_width=10)
+        curve.set_color_by_gradient(BLUE, RED)
+        
         new_curve = CurvesAsSubmobjects(curve)
-        print(len(new_curve))
-        # 这个值主要是由t_range数组的个数决定
+        print(len(new_curve))# 这个值主要是由t_range数组的个数决定
         new_curve.set_color_by_gradient(BLUE, RED)
         self.add(new_curve.shift(UP), curve)
+        
         self.wait()

94e6ae46964287ec3457620688c471c61e11f4ec
Ai Chain <hitthumit@gmail.com>
2024-01-15 16:16:36 +0800

    做好一件事 = 80%的执行套路 + 20%的创造力

--- Fourier_bk/fourier.py
+++ Fourier_bk/fourier.py
@@ -721,7 +721,7 @@ class Normal_happy(FourierCirclesSceneWithCamera):
 class NeedZoom(FourierCirclesSceneWithCamera):
     def construct(self):
         super().__init__(n_vectors=200,#控制向量数量
-        slow_factor=1/5,#控制时间长短，slow factor越小，画的速度越慢,      
+        slow_factor=1/3,#控制时间长短，slow factor越小，画的速度越慢,      
         cairo_line_width_multiple=0.005,#控制缩放镜头里线的粗细
         default_frame_stroke_width=0.1*1000,#控制缩放镜头边框粗细 #linus似乎没有效果
         zoomed_display_corner_buff=0.2)

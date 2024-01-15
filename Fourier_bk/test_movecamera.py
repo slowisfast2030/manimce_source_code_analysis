d5e33580e0eb749fc3ce14e19fe569575ec1f63f
Ai Chain <hitthumit@gmail.com>
2024-01-15 13:33:19 +0800

    做好一件事 = 80%的执行套路 + 20%的创造力

---
+++ Fourier_bk/test_movecamera.py
@@ -0,0 +1,28 @@
+from manim import *
+
+class CameraFocusExample(MovingCameraScene):
+    def construct(self):
+        # Create some objects
+        square = Square(color=BLUE, fill_opacity=0.5)
+        circle = Circle(color=RED, fill_opacity=0.5).shift(LEFT * 3)
+        triangle = Triangle(color=GREEN, fill_opacity=0.5).shift(RIGHT * 3)
+
+        # Add objects to the scene
+        self.add(square, circle, triangle)
+
+        # Focus on the square
+        self.play(self.camera.frame.animate.move_to(square).set(width=square.width * 1.2))
+
+        # Wait and then focus on the circle
+        self.wait(1)
+        self.play(self.camera.frame.animate.move_to(circle).set(width=circle.width * 1.2))
+
+        # Wait and then focus on the triangle
+        self.wait(1)
+        self.play(self.camera.frame.animate.move_to(triangle).set(width=triangle.width * 1.2))
+
+        # Zoom out to show all objects
+        self.wait(1)
+        self.play(self.camera.frame.animate.set(width=10))
+
+        self.wait(1)

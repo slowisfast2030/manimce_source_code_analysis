e0d849d046abca5c3c5bd8ac6889d762d5bb68ed
Ai Chain <hitthumit@gmail.com>
2024-01-20 16:23:26 +0800

    做好一件事 = 80%的执行套路 + 20%的创造力

--- Fourier_bk/fourier_pro.py
+++ Fourier_bk/fourier_pro.py
@@ -452,7 +452,7 @@ class Normal_happy_pro(FourierCirclesSceneWithCamera):
         return coefs,freqs
        
     def construct(self):
-        super().__init__(n_vectors=400,#控制向量数量
+        super().__init__(n_vectors=200,#控制向量数量
         slow_factor=1/3,#控制时间长短，slow factor越小，画的速度越慢,      
         cairo_line_width_multiple=0.01,#控制缩放镜头里线的长短
         default_frame_stroke_width=0.1,)#控制缩放镜头边框长短

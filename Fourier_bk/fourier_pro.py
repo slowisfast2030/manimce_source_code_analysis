072cddf9f6574749863991b53a7e9261d96180e0
Ai Chain <hitthumit@gmail.com>
2024-01-21 12:44:26 +0800

    做好一件事 = 80%的执行套路 + 20%的创造力

--- Fourier_bk/fourier_pro.py
+++ Fourier_bk/fourier_pro.py
@@ -638,4 +638,4 @@ class Normal_happy_pro_plus(FourierCirclesSceneWithCamera):
 
         svg_path = "new.svg"
         svg_path = "happynewyear.svg"
-        process_word(n_vectors=200, all_time=15, svg_path=svg_path, origin=complex(-1.8,1.5))
+        process_word(n_vectors=200, all_time=20, svg_path=svg_path, origin=complex(-1.8,1.5))
